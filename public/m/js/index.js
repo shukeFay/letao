7eb9caaedcec6664bfb92d2e2928d5b839eb5123
shukeFay <826219314@qq.com>
2019-03-03 09:41:18 +0800

    乐淘项目2.0版

---
+++ public/m/js/index.js
@@ -0,0 +1,22 @@
+$(function(){
+// 轮播图初始化
+ // 初始化swiper的轮播图
+ var swiper = new Swiper('.swiper-container', {
+    loop: true,
+    pagination: {
+        el: '.swiper-pagination',
+        clickable: true,
+    },
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+
+});
+
+mui('.mui-scroll-wrapper').scroll({
+	deceleration: 0.0005 //flick 减速系数，系数越大，滚动速度越慢，滚动距离越小，默认值0.0006
+});
+
+ });
+
